501c579bf699a6bb9dad9f573f71514c0aae5903
Beatriz Mozer Aragão <beatriz.mozer.aragao@avanade.com>
2024-05-10 11:38:54 -0300

    Adicionado Model e Controller

---
+++ ProjetoTarefas/ListaDeTarefas/ListaDeTarefas/Controllers/AssignmentController.cs
@@ -0,0 +1,39 @@
+﻿using ListaDeTarefas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ListaDeTarefas.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private static List<Assignment> _assignments = new List<Assignment>()
+        {
+            new Assignment {AssignmentId = 1, AssignmentName = "Tirar o Lixo", Description = "", StartDate = new DateTime(2024,05,10, 8, 0, 0), EndDate = new DateTime(2024,05,12, 17, 0, 0), Status = "Concluído"},
+            new Assignment {AssignmentId = 1, AssignmentName = "Projeto da 4ª Semana",Description = "", StartDate = new DateTime(2024,05,10, 9, 30, 0), EndDate = new DateTime(2024,05,25, 17, 0, 0), Status = "Concluído"},
+        };
+        public IActionResult Index()
+        {
+            return View(_assignments);
+        }
+
+        [HttpGet] 
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create (Assignment assignment)
+        {
+            if(ModelState.IsValid)
+            {
+                assignment.AssignmentId = _assignments.Count > 0 ? _assignments.Max(t => t.AssignmentId) + 1 : 1;
+                _assignments.Add(assignment);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ ProjetoTarefas/ListaDeTarefas/ListaDeTarefas/Models/Assignment.cs
@@ -0,0 +1,17 @@
+﻿namespace ListaDeTarefas.Models
+{
+    public class Assignment
+    {
+        public int AssignmentId { get; set; }
+        public string? AssignmentName { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string? Status { get; set; }
+
+    }
+}
